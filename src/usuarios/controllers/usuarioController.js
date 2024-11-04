b93e7967da3ab978177f2680102f865a07f98ed3
JeremiasONunes <jeremias.6292@gmail.com>
2024-11-04 19:13:51 -0300

    UPDATE - mapeamento de erro ao criar o usuario

--- src/usuarios/controllers/usuarioController.js
+++ src/usuarios/controllers/usuarioController.js
@@ -3,16 +3,17 @@ const usuarioService = require('../services/usuarioService');
 // Criação de novos usuários
 exports.createUsuario = async (req, res) => {
   try {
+    console.log("Dados recebidos:", req.body);
     const usuario = await usuarioService.create(req.body);
     res.status(201).json(usuario);
   } catch (error) {
-    console.error('Erro ao criar usuário:', error); 
-    res.status(500).json({ error: 'Erro ao criar usuário' });
+    console.error(`Erro ao criar usuário: `, error);
+    res.status(500).json({ error: `Erro ao criar usuário ${error.message} CPF ja cadastrado` });
   }
 }
 
 // Listagem de todos os usuários
-exports.getAllUsuarios = async (req, res) => {
+exports.getAllUsuarios = async (_req, res) => {
   try {
     const usuarios = await usuarioService.getAll();
     res.status(200).json(usuarios);
